6c92763f349320a3bd9213521830f942d83dc9d8
Nicolas MEO <nicolas.meo@tabmo.io>
2023-08-25 15:29:50 +0200

    Add helpers

--- src/main/java/com/nmeo/App.java
+++ src/main/java/com/nmeo/App.java
@@ -16,10 +16,22 @@ public class App {
         int port = System.getenv("SERVER_PORT") != null? Integer.parseInt(System.getenv("SERVER_PORT")) : 8080;
 
         Javalin.create()
-            .get("/api/status", ctx -> {
-                logger.debug("Status handler triggered", ctx);
-                ctx.status(200);
-            })
-            .start(port);
+        .get("/api/status", ctx -> {
+            logger.debug("Status handler triggered", ctx);
+            ctx.status(200);
+        })
+        .post("/api/create", ctx -> {
+            //Pokemon pokemon = ctx.bodyAsClass(Pokemon.class);
+        })
+        .get("/api/searchByName", ctx -> {
+            //String nameToSearch = ctx.queryParam("name");
+        })
+        .post("/api/modify", ctx -> {
+            //String typeToSearch = ctx.queryParam("type");
+        })
+        .get("/api/searchByType", ctx -> {
+            //ModifyPokemonRequest request = ctx.bodyAsClass(ModifyPokemonRequest.class);
+        })
+        .start(port);
     }
 }
